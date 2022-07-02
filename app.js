9ba1f479da010c8b2e58fadb5d12110efa82c493
Suvendu Bissoyi <Suvendu_Bissoyi@gep.com>
2022-07-02 19:26:05 +0530

    start

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const https=require('https');
+
+app=express();
+
+app.use(express.static('public'));
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"index.html");
+})
+
+app.listen(3000,function(){
+    console.log("sever is running!!");
+});
