e84596a44f7302f391277dd00dad41394920f831
Suvendu Bissoyi <suvendu_bissoyi@gep.com>
2022-07-03 15:01:25 +0530

    style.css separated

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ app=express();
 
 app.use(express.static('public'));
 
-
 app.get("/",function(req,res){
     res.sendFile(__dirname+"index.html");
-    console.log('hi');
 })
-
-console.log("hi to the world");
 
 app.listen(3000,function(){
     console.log("sever is running!!");
